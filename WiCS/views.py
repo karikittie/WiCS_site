3ed3331f876251a6ad58e1f162329f6cbe50aeb3
Karis Sponsler <karis4@pdx.edu>
2017-03-27 11:46:56 -0700

    init

---
+++ WiCS/views.py
@@ -0,0 +1,13 @@
+import os
+
+from django.shortcuts import render_to_response
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_STATIC = os.path.join(BASE_DIR, "static")
+
+
+def home(request):
+    staticCSS = os.listdir(os.path.join(BASE_STATIC, "CSS"))
+    staticCSS = [file for file in staticCSS if file.endswith(".css")]
+    staticJS = os.listdir(os.path.join(BASE_STATIC, "JS"))
+    return render_to_response('index.html', {'title': 'home', 'staticCSS': staticCSS, 'staticJS': staticJS})
